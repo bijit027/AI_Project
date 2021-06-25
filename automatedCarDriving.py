e26032363d91bbd701692384105dab59e5d10699
bijit027 <bijitdeb70@gmail.com>
2021-06-25 10:06:05 +0600

    pedestrians

--- automatedCarDriving.py
+++ automatedCarDriving.py
@@ -3,12 +3,18 @@ import cv2
 # Image file
 img_file = 'cars-in-middle-of-street-road.jpg'
 video = cv2.VideoCapture('video.mp4')
+#video = cv2.VideoCapture('traffic_street_cars_turning_evening_602.mp4')
+#video = cv2.VideoCapture('video (1).mp4')
+#video = cv2.VideoCapture('production ID_4833483.mp4')
 
 #Pre-trained car classifier
 classifier_file = 'car_detector.xml'
+pedestrian_tracker_file = 'pedestrian.xml'
 
 #Create car classifier
 car_tracker = cv2.CascadeClassifier(classifier_file)
+pedestrian_tracker = cv2.CascadeClassifier(pedestrian_tracker_file)
+
 
 
 
@@ -24,26 +30,36 @@ while True:
     else: 
        break
 
-    #detect cars
+    #detect cars and pedestrians
     cars = car_tracker.detectMultiScale(grayscaled_frame)
+    pedestrians = pedestrian_tracker.detectMultiScale(grayscaled_frame)
 
     #Draw rectangles around the cars
     for(x,y,w,h) in cars:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255),2)
+        abc =  cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255),2)
+        cv2.putText(abc, 'Car', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12), 2)
 
     
-
+    #Draw rectangles around the pedestrians
+    for(x,y,w,h) in pedestrians:
+        abc =  cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,255),2)
+        cv2.putText(abc, 'Pedestrian', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
 
 
 
     #Display  the image with the faces spotted
-    cv2.imshow('Car Detector',frame)
+    cv2.imshow('Self Driving Car',frame)
 
     #Dont autoclose (Wait here in the code and listen for a key press)
-    cv2.waitKey(1)
+    key =  cv2.waitKey(1)
+
+    #Stop if Q is pressed
+    if key == 81 or key == 113:
+        break
 
 
-
+#Release the videocapture object
+video.release()
 """
 
 #create opencv image
@@ -79,7 +95,7 @@ cv2.waitKey()
 
 
 
-
+#for run this project -> python automatedCarDriving.py
 
 
 """
